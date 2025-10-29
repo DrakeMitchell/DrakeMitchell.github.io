032ae691f83f576c6f90534dd22d48219eca8889
DrakeMitchell <mitchelld7373@my.uwstout.edu>
2025-10-28 20:39:58 -0500

    Working on Buttons for filtering projects

---
+++ ProjectFilters.js
@@ -0,0 +1,5 @@
+﻿const button = document.querySelector('add');
+button.addEventListener('click', () => {
+    const element = document.querySelector('Games');
+    element.style.backgroundColor = 'red';
+});
