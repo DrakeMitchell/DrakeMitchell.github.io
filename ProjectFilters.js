9d3aa7f57d2d66b10be6ad36eeb7050d7c77c2a5
DrakeMitchell <mitchelld7373@my.uwstout.edu>
2025-10-29 11:39:46 -0500

    Lots of work for filtering buttons and Pallete selection

--- ProjectFilters.js
+++ ProjectFilters.js
@@ -1,5 +1,76 @@
-﻿const button = document.querySelector('add');
+﻿const AllButton = document.querySelector('All');
+const GamesButton = document.querySelector('Games');
+const SoftwareButton = document.querySelector('Software');
+const SoloButton = document.querySelector('Solo');
 button.addEventListener('click', () => {
     const element = document.querySelector('Games');
     element.style.backgroundColor = 'red';
 });
+
+AllButton.addEventListener('click', All)
+function All(){
+    const element = document.querySelectorAll('.Games');
+    element.forEach(el => {
+        el.style.display = "block"
+    });
+
+    const element2 = document.querySelectorAll('.Software');
+    element2.forEach(el => {
+        el.style.display = "block"
+    });
+}
+
+function Games(){
+    All()
+    const element = document.querySelectorAll('.Software');
+    element.forEach(el => {
+        if (el.style.display != "none") {
+            el.style.display = "none";
+        } else {
+            el.style.display = "block";
+        }
+    })
+}
+
+function Software(){
+    All()
+    const element = document.querySelectorAll('.Games');
+    element.forEach(el => {
+        if (el.style.display != "none") {
+            el.style.display = "none";
+        } else {
+            el.style.display = "block";
+        }
+    })
+}
+
+function Solo(){
+    All()
+    for(var i = 0; i < 2; i++){
+        if (i == 0) {
+            var element = document.querySelectorAll('.Games');
+        }
+        else if (i == 1) {
+            element = document.querySelectorAll('.Software');
+        }
+        element.forEach(el => {
+            if (el.style.display != "none") {
+                el.style.display = "none";
+            }
+            else {
+                el.style.display = "block";
+            }
+        })
+    }
+
+    const element1 = document.querySelectorAll('.Solo');
+    element1.forEach(el => {
+        el.style.display = "block";
+    })
+}
+
+
+AllButton.onclick = All;
+GamesButton.onclick = Games;
+SoftwareButton.onclick = Software;
+SoloButton.onclick = Solo;
