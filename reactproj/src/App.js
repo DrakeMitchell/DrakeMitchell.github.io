28f88ae57756fcd3e0a6298e1cf286a28dfc22f7
Drake Mitchell <drakefmitchell@protonmail.com>
2026-07-17 18:48:56 -0500

    problem fixed?

--- reactproj/src/App.js
+++ reactproj/src/App.js
@@ -19,7 +19,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route path='/#' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/#/resume' element={<AboutMe/>}/>
         <Route path='/#/projects' element={<Portfolio/>}/>
 
