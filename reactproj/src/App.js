66ff0ee62dfb5ad5f4064ff92b85a756f4d9d34f
Drake Mitchell <drakefmitchell@protonmail.com>
2026-07-17 18:15:01 -0500

    Hashrouter

--- reactproj/src/App.js
+++ reactproj/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Home from './Pages/Home/Homescreen/HomePage.jsx';
 import Navbar from './Pages/Home/Homescreen Components/Navbar';
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Routes,
     Route,
 } from "react-router-dom";
