1d0eb84cd8113174211d5f85bb3c441cf858033c
Drake Mitchell <drakefmitchell@protonmail.com>
2026-07-17 21:36:59 -0500

    Adding Portfolio Website project

---
+++ reactproj/src/Pages/Home/Projects/ProjectData/PortfolioWebsite.js
@@ -0,0 +1,52 @@
+export const PortfolioWebsite = {
+    // Header
+    title:"This Website!",
+    subtitle:"React + CSS",
+    genre:"",
+
+    //Hero
+    poster:"",
+    releaseDate:"",
+    studioName:"",
+    teamSize:"Solo",
+    platforms:"PC",
+    responsiblities:[
+
+    ],
+    engine:"",
+    projectLength:"",
+
+
+    //Description
+    desc:[
+
+    ],
+    img:[
+        
+    ],
+    contributions:[
+
+    ],
+
+    //Home Screen Hero
+    thumbnail:"",
+    shortDesc:``,
+    badge:"",
+    url:"/projects/portfolio website",
+    type:"Software Solo",
+
+
+    //Special Sections
+    downloadable:"",
+    credits:"",
+    awards:"",
+    awardImg:[
+
+    ],
+    software:"yes",
+
+    //Links
+    githubLink:"",
+    itchioLink:""
+
+}
--- reactproj/src/Pages/Home/Projects/ProjectData/ProjectOrder.js
+++ reactproj/src/Pages/Home/Projects/ProjectData/ProjectOrder.js
@@ -4,12 +4,14 @@ import {FullStar} from "./FullStar"
 import { GatorHucker} from "./GatorHucker"
 import {LSystem} from "./L-System"
 import { StickySituation } from "./Sticky Situation"
+import { PortfolioWebsite } from "./PortfolioWebsite"
 
 export const ProjectOrder={
         projects:[
             GatorHucker,
             Awwmageddon,
             StickySituation,
+            PortfolioWebsite,
             LSystem,
             Obsession,
             FullStar
