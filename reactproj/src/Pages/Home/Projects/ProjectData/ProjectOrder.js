b888bf5617cd59b670040822bf513c714a579c39
Drake Mitchell <drakefmitchell@protonmail.com>
2026-07-17 21:39:18 -0500

    Disabled Portfolio Website
    Will reactivate once it is better completed

--- reactproj/src/Pages/Home/Projects/ProjectData/ProjectOrder.js
+++ reactproj/src/Pages/Home/Projects/ProjectData/ProjectOrder.js
@@ -11,7 +11,7 @@ export const ProjectOrder={
             GatorHucker,
             Awwmageddon,
             StickySituation,
-            PortfolioWebsite,
+            //PortfolioWebsite,
             LSystem,
             Obsession,
             FullStar
